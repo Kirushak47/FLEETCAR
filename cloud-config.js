85f79fbce4ba9246be82e7bf477c1bffb7e0659b
Kirushak47 <balyshevy@gmail.com>
2026-08-05 14:51:30 +0200

    Update cloud-config.js

--- cloud-config.js
+++ cloud-config.js
@@ -11,7 +11,7 @@
 window.FLEETPILOT_CLOUD_CONFIG = Object.freeze({
   url: "https://tbpfasumklpdqwnlfncd.supabase.co",
   publishableKey: "sb_publishable_Y8lMZQN7Fc7V2HAixaZZrA_w-7STzMh",
-  ownerEmail: "Balyshevy@gmail.com",
+  ownerEmail: "balyshevy@gmail.com",
   redirectUrl: "https://kirushak47.github.io/FLEETCAR/?email-confirmed=1",
   dashboardUrl: "https://supabase.com/dashboard/project/tbpfasumklpdqwnlfncd"
 });
