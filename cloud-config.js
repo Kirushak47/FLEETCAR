a537d17c890ae3011cf96ed6481943dab8f6377d
Kirushak47 <balyshevy@gmail.com>
2026-08-06 15:17:06 +0200

    Delete cloud-config.js

--- cloud-config.js
+++
@@ -1,7 +0,0 @@
-window.FLEETPILOT_CLOUD_CONFIG = Object.freeze({
-  url: "https://tbpfasumklpdqwnlfncd.supabase.co",
-  publishableKey: "sb_publishable_Y8lMZQN7Fc7V2HAixaZZrA_w-7STzMh",
-  ownerEmail: "balyshevy@gmail.com",
-  redirectUrl: "https://kirushak47.github.io/FLEETCAR/?email-confirmed=1",
-  dashboardUrl: "https://supabase.com/dashboard/project/tbpfasumklpdqwnlfncd"
-});
