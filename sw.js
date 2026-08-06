55dd09e8ffd0930b0abf5b03114a39a45c1eefe1
Kirushak47 <balyshevy@gmail.com>
2026-08-06 14:21:28 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const C="fleetpilot-v11-4-vehicle-status-panel";
-const A=["./","./index.html","./styles.css?v=114000","./app.js?v=114000","./cloud-config.js?v=114000","./cloud.js?v=114000","./manifest.webmanifest?v=114000"];
+const C="fleetpilot-crm-12-0";
+const A=["./","./index.html","./styles.css?v=120000","./app.js?v=120000","./cloud-config.js?v=120000","./cloud.js?v=120000","./manifest.webmanifest?v=120000"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
