56628dab7915cda6618ab97cb340dd895a63330f
Kirushak47 <balyshevy@gmail.com>
2026-08-06 11:18:02 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,1 @@
-const C="fleetpilot-v10-8-1-period-profit";
-const A=["./","./index.html","./styles.css?v=108100","./app.js?v=108100","./cloud-config.js?v=108100","./cloud.js?v=108100","./manifest.webmanifest?v=108100"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C="fleetpilot-v10-9-insurance";const A=["./","./index.html","./styles.css?v=109000","./app.js?v=109000","./cloud-config.js?v=109000","./cloud.js?v=109000","./manifest.webmanifest?v=109000"];self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
