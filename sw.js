1b815941910d2e3af9ade660851923c01f2a97ae
Kirushak47 <balyshevy@gmail.com>
2026-08-06 15:22:30 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const C="fleetpilot-v11-3-1-finance-service-fix";
+const A=["./","./index.html","./styles.css?v=113100","./app.js?v=113100","./cloud-config.js?v=113100","./cloud.js?v=113100","./manifest.webmanifest?v=113100"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
