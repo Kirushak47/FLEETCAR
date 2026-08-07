72cb8c0ccacde463d3b8c32851aa72edc7c8f3e4
Kirushak47 <balyshevy@gmail.com>
2026-08-07 13:05:25 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="fleetpilot-v13-1-service-hotfix";
-const ASSETS=["./","./index.html","./styles.css?v=131000","./app.js?v=131000","./cloud-config.js?v=131000","./cloud.js?v=131000","./manifest.webmanifest?v=131000"];
+const CACHE="fleetpilot-v13-1-1-service-layout-fix";
+const ASSETS=["./","./index.html","./styles.css?v=131100","./app.js?v=131100","./cloud-config.js?v=131100","./cloud.js?v=131100","./manifest.webmanifest?v=131100"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",event=>{if(event.request.method!=="GET")return;event.respondWith(fetch(event.request).catch(()=>caches.match(event.request)))});
