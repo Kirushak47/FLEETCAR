834967bff5fca08b066d493a03725998b7394796
Kirushak47 <balyshevy@gmail.com>
2026-08-07 17:26:15 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE="fleetpilot-v16-0-mobile-ui";
+const CACHE="fleetpilot-v17-0-enterprise-mobile-ui";
 const ASSETS=[
   "./","./index.html","./manifest.webmanifest",
-  "./fp-base.css?v=158000","./fp-service-layout.css?v=158000","./fp-desktop-gps.css?v=158000","./fp-cloud-roles.css?v=158000","./fp-driver.css?v=158000","./fp-crm-service.css?v=158000","./fp-current-ui.css?v=158000","./fp-mobile-v16.css?v=160000",
+  "./fp-base.css?v=158000","./fp-service-layout.css?v=158000","./fp-desktop-gps.css?v=158000","./fp-cloud-roles.css?v=158000","./fp-driver.css?v=158000","./fp-crm-service.css?v=158000","./fp-current-ui.css?v=158000","./fp-mobile-v17.css?v=170000",
   "./cloud-config.js?v=158000","./cloud.js?v=158000",
-  "./fp-core-data.js?v=158000","./fp-roles-company.js?v=160000","./fp-driver-portal.js?v=158000","./fp-router-navigation.js?v=158000","./fp-files-backups.js?v=158000","./fp-analytics-dashboard.js?v=158000","./fp-gps-map.js?v=158000","./fp-fleet.js?v=158000","./fp-service-finance.js?v=158000","./fp-calendar-vehicle.js?v=158000","./fp-actions-documents.js?v=158000","./fp-boot-hotfixes.js?v=158000"
+  "./fp-core-data.js?v=158000","./fp-roles-company.js?v=170000","./fp-driver-portal.js?v=158000","./fp-router-navigation.js?v=158000","./fp-files-backups.js?v=158000","./fp-analytics-dashboard.js?v=158000","./fp-gps-map.js?v=158000","./fp-fleet.js?v=158000","./fp-service-finance.js?v=158000","./fp-calendar-vehicle.js?v=158000","./fp-actions-documents.js?v=158000","./fp-boot-hotfixes.js?v=158000"
 ];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
